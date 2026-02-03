1c2bfeee85c35f67d5f60a9d46343566ff759c5b
fawda123 <mbafs2012@gmail.com>
2026-02-03 13:05:10 -0500

    add globalVariables and stats imports, add GPL3 license, update NAMESPACE

---
+++ R/globalVariables.R
@@ -0,0 +1,5 @@
+globalVariables(c("dropList", "inputna", "mmpred_", "mmse_", "se_plot_", "x_plot_", 
+  "xpndMat", "y_plot_"))
+
+#' @importFrom stats as.formula model.frame model.matrix model.offset model.response napredict qnorm setNames
+NULL
